44b68045d613f48fad284ac21b2cd721fd8d88cd
juanfariasdev <juanpablo.profissional.a.f@gmail.com>
2021-11-09 22:04:24 -0300

    adicionei alguns comentarios

--- checkpoin.js
+++ checkpoin.js
@@ -7,6 +7,8 @@ const TempoNecessario = 20;
 //Coloque False caso não queira que apareçã o menu
 var aparecerMenu = true;
 
+
+//Escolhi Array pois posso adicionar mais coisas e automaticamente ir para o menu
 var pratos = [
     {
         id: 1,
@@ -40,7 +42,8 @@ var existeItem = false;
 function Menu() {
     if (aparecerMenu) {
         console.log("------------ MENU -----------")
-        pratos.map(function (item, indice) {
+        //optei pelo .map pois fica mais Facil adicionar itens ao menu
+        pratos.map(function (item, key) {
             console.log(`${item.id} - ${item.Title} - ${item.tempo} segundos`);
         })
         console.log("------------  *  ------------")
@@ -60,10 +63,12 @@ function SelecionarProduto(opcao, tempo) {
             console.log('Tempo Insuficiente');
             msgExtra = "Pena que deve está frio ;-;";
         }
+        //Considerei que em 3x o Microondas já explode
         if (tempo >= (tempoProduto * 3)) {
             console.log('KABUUMMM');
             msgExtra = "Deve ser dificil comer assim...";
         }
+        //Considerei que em 2x ela queima.
         if (tempo >= (tempoProduto * 2) && tempo < (tempoProduto * 3)) {
             console.log(`Você queimou o ${comidaMicrondas}`);
             msgExtra = "Acho que errou o ponto em... ;-;";
@@ -74,7 +79,7 @@ function SelecionarProduto(opcao, tempo) {
 
 
     }
-    pratos.map(function (item, indice) {
+    pratos.map(function (item, key) {
         if (item.id === opcao) {
             existeItem = true;
             tempoProduto = item.tempo;
@@ -97,11 +102,10 @@ function SelecionarProduto(opcao, tempo) {
 
 function microOndas() {
     Menu()
-
+    SelecionarProduto(opcaoEscolhida, TempoNecessario)
 }
 
 
 //Exibe o MENU
 microOndas()
 
-SelecionarProduto(opcaoEscolhida, TempoNecessario)
